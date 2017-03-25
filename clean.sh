ae2dae3f16011a5d765ad43a1a90fcf909267897
James Livermont <jlivermont@expansioncapitalgroup.com>
2017-03-25 01:29:59 -0500

    Updates

--- clean.sh
+++ clean.sh
@@ -2,8 +2,8 @@
 
 set -x
 
-docker rm -fv `docker ps -a -q -f status=exited`
-docker rmi -f `docker images -f 'dangling=true' -q`
-wget -O /tmp/docker-cleanup-volumes.sh https://raw.githubusercontent.com/chadoe/docker-cleanup-volumes/master/docker-cleanup-volumes.sh
+docker rm -fv $(docker ps -a -q -f status=exited)
+docker rmi -f $(docker images -f 'dangling=true' -q)
+wget --no-check-certificate -O /tmp/docker-cleanup-volumes.sh https://raw.githubusercontent.com/chadoe/docker-cleanup-volumes/master/docker-cleanup-volumes.sh
 sudo -i /bin/bash /tmp/docker-cleanup-volumes.sh
 sudo -i /bin/bash -c 'truncate --size=<512M /var/lib/docker/containers/**/*-json.log'
