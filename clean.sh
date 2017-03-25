6f8df1f681080af578f9b7b84c49cca7e68a6a97
James Livermont <jlivermont@expansioncapitalgroup.com>
2017-03-25 01:43:35 -0500

    Escpaed less than sign

--- clean.sh
+++ clean.sh
@@ -6,4 +6,4 @@ docker rm -fv $(docker ps -a -q -f status=exited)
 docker rmi -f $(docker images -f 'dangling=true' -q)
 wget --no-check-certificate -O /tmp/docker-cleanup-volumes.sh https://raw.githubusercontent.com/chadoe/docker-cleanup-volumes/master/docker-cleanup-volumes.sh
 sudo -i /bin/bash /tmp/docker-cleanup-volumes.sh
-sudo -i /bin/bash -c 'truncate --size=<512M /var/lib/docker/containers/**/*-json.log'
+sudo -i /bin/bash -c 'truncate --size=\<512M /var/lib/docker/containers/**/*-json.log'
